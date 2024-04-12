7e37c058e97060048bc86579291df6e84658fa2b
melo.lin <melo.lin@appier.com>
2024-04-12 18:01:01 +0800

    feature: update index.js

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-// index.js
-const line = require('@line/bot-sdk');
-var express = require('express');
+import * as line from '@line/bot-sdk'
+import express from 'express'
+
+// create LINE SDK config from env variables
 const config = {
-  channelAccessToken: 'Y0PMV50P3nuZa9D92xS+m2n0FeKxfyd9ux5mqa+/5gXUOiIprgeBOeJ2qK8DXdP3jP5wqcnOiMtztWBazmsQiqWz+EJpkQiJ0S2KEA/CA+gSmPauUiox1ZtyrIP5zcgSt6Y2iQSTf7dbB7Uli5qTkwdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '3eb492079914e41fc4baef8b7965021c'
+  channelSecret: process.env.CHANNEL_SECRET,
 };
+
 // create LINE SDK client
-const client = new line.Client(config);
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
 // create Express app
-// about Express itself: <https://expressjs.com/>
+// about Express itself: https://expressjs.com/
 const app = express();
+
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
-	console.log(req, res)
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
@@ -22,17 +26,24 @@ app.post('/callback', line.middleware(config), (req, res) => {
       res.status(500).end();
     });
 });
+
 // event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
-  // create a echoing text message
+
+  // create an echoing text message
   const echo = { type: 'text', text: event.message.text };
+
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  return client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [echo],
+  });
 }
+
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
